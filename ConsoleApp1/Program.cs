51d751815a3e32247bbe21810e67ba40316c07b4
Kuan Tsai <ktsai11486@gmail.com>
2022-11-22 22:56:49 +0800

    換成質數來除

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,20 +7,41 @@ while (true)
     if (inpute == "exit")
         break;
     int num = 0;
-    
+    int[] check = new int[] {2,3,5,7,11,13,17,19,23,29,
+                             31,37,41,43,47,53,59,61,67,71,
+                             73,79,83,89,97};
     if (int.TryParse(inpute, out num))
     {
         bool result = false;
         if (num % 2 != 0)
         {
 
-            for (int i = 3; i < (num > 100 ? 100 : num - 1); i += 2)
-            {
+            //for (int i = 3; i < (num > 100 ? 100 : num - 1); i += 2)
+            //{
 
-                if (num % i == 0)
+            //    if (num % i == 0)
+            //    {
+            //        result = true;
+            //        break;
+            //    }
+            //}
+            for (int i = 0; i < check.Length; i++)
+            {
+                if (num % check[i] == 0)
                 {
+                    if (num == check[i])
+                    {
+                        break;
+                    }
                     result = true;
                     break;
+                }
+                else
+                {
+                    if (num / check[i] == check[i])
+                    {
+                        break;
+                    }
                 }
             }
         }
