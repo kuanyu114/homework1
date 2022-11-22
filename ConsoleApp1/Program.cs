6b83ef3441904785b66794355adb3c5496eb336b
Kuan Tsai <ktsai11486@gmail.com>
2022-11-22 22:15:15 +0800

    排除偶數 檢查奇數

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,25 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-string inpute = Console.ReadLine();
-int num = 0;
-if (int.TryParse(inpute, out num))
+
+while (true)
 {
-    bool result = false;
-    for (int i = 2; i < (num >100 ? 100: num-1); i++)
+    string inpute = Console.ReadLine();
+    if (inpute == "exit")
+        break;
+    int num = 0;
+    
+    if (int.TryParse(inpute, out num))
     {
-       
-        if (num % i == 0)
+        bool result = false;
+        if (num % 2 != 0)
         {
-            result = true;
-            break;
+
+            for (int i = 3; i < (num > 100 ? 100 : num - 1); i += 2)
+            {
+
+                if (num % i == 0)
+                {
+                    result = true;
+                    break;
+                }
+            }
         }
+        else
+        {
+            if (num != 2)
+                result = true;
+        }
+    
+        if (result)
+            Console.WriteLine(num + "不是質數");
+        else
+            Console.WriteLine(num + "是質數");
     }
-    if (result)
-        Console.WriteLine(num + "不是質數");
     else
-        Console.WriteLine(num + "是質數");
-}
-else
-{
-    Console.WriteLine("請輸入數字");
+    {
+        Console.WriteLine("請輸入數字");
+    }
 }
