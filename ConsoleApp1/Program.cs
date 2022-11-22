e9e4d953d0416004d3153626dc4bde2a9c4b603b
a9246 <a9246@KUAN>
2022-11-22 20:36:54 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+string inpute = Console.ReadLine();
+int num = 0;
+if (int.TryParse(inpute, out num))
+{
+    bool result = false;
+    for (int i = 2; i < num - 1; i++)
+    {
+       
+        if (num % i == 0)
+        {
+            result = true;
+            break;
+        }
+    }
+    if (result)
+        Console.WriteLine(num + "是質數");
+    else
+        Console.WriteLine(num + "不是質數");
+}
+else
+{
+    Console.WriteLine("請輸入數字");
+}
